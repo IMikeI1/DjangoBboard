6fb2398f3d9d753fa2687b66e316151d8c87fd55
IMikeI1 <3249541@mail.ru>
2025-05-13 20:40:44 +0300

    django_ads

--- BboardSite/settings.py
+++ BboardSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fbjb*_21-qag^&&b)$niw$z2wlmy50wg99t$_vhopa5)6e9^3w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
